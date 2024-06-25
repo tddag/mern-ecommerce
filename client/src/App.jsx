73e96f8a97a1667a74042306f84b745990c43361
Tam Dang <danhtam0509@gmail.com>
2024-06-24 23:22:45 -0400

    work on singple product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Products } from './pages/Products'
 import { ManageProducts } from './pages/ManageProducts'
 import { Home } from './pages/Home'
+import { IndividualProduct } from './pages/IndividualProduct'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/manageProducts" element={<ManageProducts/>}/>
+        <Route path="/product" element={<IndividualProduct/>}/>
       </Routes>
     </>
   )
